2c46bf12b7b991bced5d8dbc7ff94e53f160a506
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-08 00:57:28 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
          #association
          #has_many :items
          #has_many :orders
+
+         #validates :nickname, presence: true
+         #validates :email, presence: true
+         #validates :password, presence: true
+         #validates :last_name, presence: true
+         #validates :first_name, presence: true
+         #validates :kana_last_name, presence: true
+         #validates :kana_first_name, presence: true
+         #validates :birth_day, presence: true
+
+
+
 end
