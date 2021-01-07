6080d587738feaf28d701873da79289b11df9ff9
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-08 01:03:43 +0900

    Revert "新規登録"
    This reverts commit 2c46bf12b7b991bced5d8dbc7ff94e53f160a506.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,23 +2,5 @@ class ItemsController < ApplicationController
 
   def index
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :kana_last_name, :kana_first_name, :birth_day)
-  end
-
+  
 end
