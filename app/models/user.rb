c7742c2d5d3f5aa3948c18b9d298c4277934c934
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-08 12:36:07 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
          #association
          #has_many :items
          #has_many :orders
+
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :encrypted_password, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :kana_last_name, presence: true
+         validates :kana_first_name, presence: true
+         validates :birth_day, presence: true
+
 end
