77d786cf1b8c4c81405e67fd729ca3338abb56db
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-11 19:26:48 +0900

    修正コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :kana_last_name, :kana_first_name, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birth_day])
   end
 end
