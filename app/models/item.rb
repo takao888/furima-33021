f07d092d918179ffe580e29eed8327d4328984bb
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-13 12:11:06 +0900

    ドロップリスト実装　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  #association
+  belongs_to :user
+  #has_one :order
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
@@ -7,18 +10,15 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
 
-  #association
-  belongs_to :user
-  #has_one :order
 
   with_options presence: true do
   validates :name
   validates :description
   validates :category_id, numericality: { other_than: 1 } 
-  validates :sales_status_id
-  validates :shipping_fee_status_id
-  validates :prefecture_id
-  validates :scheduled_delivery_id
+  validates :sales_status_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
   validates :price
   validates :user_id
   end
