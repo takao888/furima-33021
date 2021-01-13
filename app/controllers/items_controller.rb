e989313bae41066ff19822795eb1cb1a71e81c9f
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-13 19:45:58 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @item = Item.all
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+  
 end
