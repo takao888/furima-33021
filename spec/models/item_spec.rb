d539d5f5dbf0a868f4268cb7f5c3924649a8fdbe
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-14 20:06:08 +0900

    修正コミット

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,8 +28,20 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Category Select")
     end
 
+    it "category_idは空だとng" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
     it "sales_status_idは1だとng" do
       @item.sales_status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status Select")
+    end
+
+    it "sales_status_idは空だとng" do
+      @item.sales_status_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Sales status Select")
     end
@@ -39,15 +51,30 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee status Select")
     end
+    it "shipping_fee_status_idは空だとng" do
+      @item.shipping_fee_status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee status Select")
+    end
 
     it "prefecture_idは1だとng" do
       @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture Select")
     end
+    it "prefecture_idは1だとng" do
+      @item.prefecture_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
+    end
 
     it "scheduled_delivery_idは1だとng" do
       @item.scheduled_delivery_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Scheduled delivery Select")
+    end
+    it "scheduled_delivery_idは空だとng" do
+      @item.scheduled_delivery_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Scheduled delivery Select")
     end
@@ -65,19 +92,19 @@ RSpec.describe Item, type: :model do
     end
 
     it "priceが全角文字だとng" do
-      @item.price = ３００
+      @item.price = "３００"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceが英数混合だとng" do
-      @item.price = pp00
+      @item.price = "pp00"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceが半角英語だけではng" do
-      @item.price = pppp
+      @item.price = "pppp"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
