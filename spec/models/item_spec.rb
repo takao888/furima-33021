e5600a8c2a96a691b05b1abbc7ddee1ceb42aba4
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-14 19:30:46 +0900

    修正コミット

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,61 +23,61 @@ RSpec.describe Item, type: :model do
     end
 
     it "category_idは1だとng" do
-      @item.category_id = "1"
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category Select")
     end
 
     it "sales_status_idは1だとng" do
-      @item.sales_status_id = "1"
+      @item.sales_status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Sales status Select")
     end
 
     it "shipping_fee_status_idは1だとng" do
-      @item.shipping_fee_status_id = "1"
+      @item.shipping_fee_status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee status Select")
     end
 
     it "prefecture_idは1だとng" do
-      @item.prefecture_id = "1"
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture Select")
     end
 
     it "scheduled_delivery_idは1だとng" do
-      @item.scheduled_delivery_id = "1"
+      @item.scheduled_delivery_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Scheduled delivery Select")
     end
 
     it "priceは299以下だとng" do
-      @item.price = "299"
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceは9999999を超過するとng" do
-      @item.price = "10000000"
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceが全角文字だとng" do
-      @item.price = "３００"
+      @item.price = ３００
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceが英数混合だとng" do
-      @item.price = "pp00"
+      @item.price = pp00
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
 
     it "priceが半角英語だけではng" do
-      @item.price = "pppp"
+      @item.price = pppp
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
