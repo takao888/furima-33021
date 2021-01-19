f56123f17e18d337887384100859d939d13627a1
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-19 21:00:30 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders
+  resources :items do
+    resources :orders
+  end
   
 end
