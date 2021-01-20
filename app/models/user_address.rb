c41559ab0355d9885eb5d95177b93b989f928069
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-21 00:27:42 +0900

    バリデーション

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,9 +1,9 @@
 class UserAddress < ApplicationRecord
 
   belongs_to :order
-  
+
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
     validates :city
     validates :address
     validates :phone_number
