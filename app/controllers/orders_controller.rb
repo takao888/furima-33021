203d18e84b0ae63fb6ab0278ba3b5b8f4fe26d45
takao888 <75712160+takao888@users.noreply.github.com>
2021-01-27 12:03:27 +0900

    修正コミット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :move_to_index, only: :index
 
   def index
--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     prefecture_id           {2}
     city                    {"名古屋市"}
     address                 {"5-1-3"}
-    building                {1}
+    building                {"1"}
     phone_number            {"09012341234"}
     token                   {"tok_abcdefghijk00000000000000000"}
   end
